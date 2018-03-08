982f9326762133113d985b407b8697a1cfaf451d
Nagra, Vijay <Vijay.X.Nagra.-ND@disney.com>
2018-03-08 11:02:40 -0800

    Fixing imports for test cases
    These have not been implemented yet

--- app/src/androidTest/java/com/vijendernagra/googlebookssearch/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/vijendernagra/googlebookssearch/ExampleInstrumentedTest.java
@@ -2,19 +2,19 @@ package com.vijendernagra.googlebookssearch;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.*;
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(JUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
